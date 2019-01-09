cd39108108d365c59bfbd0618ae881f728342d88
TaeGooHan <37430535+xorn2314@users.noreply.github.com>
2019-01-09 12:41:48 +0900

    애니메이션, 적멈춤

---
+++ 개발 중간/Assets/Script/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void MapScene() //지도
+    {
+        SceneManager.LoadScene("MapScene");
+    }
+
+    public void HomeScene() //아지트
+    {
+        SceneManager.LoadScene("HomeScene");
+    }
+
+}
---
+++ 개발 중간/Assets/Script/CreatureMovement.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreatureMovement : MonoBehaviour
+{
+    public float movePower = 1f;        //이동속도
+
+    Vector3 movement;
+    int movementFlag = 0;
+    bool isTracing;
+    bool isMoving;
+    GameObject traceTaget;
+    GameObject Player;
+    GameObject Enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ChangeMovement");
+        Player = GameObject.Find("Player");
+        Enemy = GameObject.Find("enemy_robot");
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(1, 3);
+
+        yield return new WaitForSeconds(3);
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            traceTaget = other.gameObject;
+
+
+            StopCoroutine("ChangeMovemnet");
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            isTracing = true;
+            movePower += 1;
+            isMoving = true;
+            if(movePower >= 100)
+            {
+                movePower = 100;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isTracing = false;
+            isMoving = false;
+            StartCoroutine("ChangeMovement");
+            movePower = 50;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        Vector3 Player_Move = Player.transform.position;
+        Vector3 Enemy_Move = Enemy.transform.position;
+
+        if (isTracing)
+        {
+            Vector3 playerPos = traceTaget.transform.position;
+
+            if (playerPos.x < transform.position.x)
+                movementFlag = 1;
+            else if (playerPos.x > transform.position.x)
+                movementFlag = 2;
+        }
+        else
+        {
+            if (movementFlag == 1)
+                movementFlag = 1;
+            else if (movementFlag == 2)
+                movementFlag = 2;
+        }
+
+        if (movementFlag == 1)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(20, 20, 1);
+        }
+
+        else if(movementFlag == 2)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-20, 20, 1);
+        }
+
+        //플레이어와 일정거리에서 마주칠 시 , movePower을 0 으로 설정
+
+        float distance = Vector3.Distance(Player.transform.position, Enemy.transform.position);
+
+        if(distance < 80 && isMoving == true)
+        {
+            movePower = 0;
+        }
+        else if(distance > 81 && isMoving == false)
+        {
+            movePower = 50;
+        }
+
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+}
