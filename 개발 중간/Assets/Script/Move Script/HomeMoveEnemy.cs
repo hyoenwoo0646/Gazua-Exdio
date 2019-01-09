5ce9fd9c691f1cc16f6042e4a20bc748e87b115c
TaeGooHan <37430535+xorn2314@users.noreply.github.com>
2019-01-09 17:42:14 +0900

    enemy 가정집으로 이동  플레이어 대기모션 추가, 걷기모션 수정

--- 개발 중간/Assets/Script/Move Script/HomeMoveEnemy.cs
+++ 개발 중간/Assets/Script/Move Script/HomeMoveEnemy.cs
@@ -17,6 +17,6 @@ public class HomeMoveEnemy : MonoBehaviour
         Vector3 pPos = this.transform.position;
 
         if(pPos.y >=-95.0f && pPos.y <= -91.0f)  //1층 이동제한
-            enemy_robot.transform.position = new Vector3(Mathf.Clamp(pPos.x, -432.5f, 389.04f), pPos.y, pPos.z);
+            enemy_robot.transform.position = new Vector3(Mathf.Clamp(pPos.x, -379.66f, 369.05f), pPos.y, pPos.z);
     }
 }
--- 개발 중간/Assets/Script/Move Script/HomePMove.cs
+++ 개발 중간/Assets/Script/Move Script/HomePMove.cs
@@ -17,21 +17,21 @@ public class HomePMove : MonoBehaviour
     {
         this.player = GameObject.FindGameObjectWithTag("Player");
         Vector3 pPos = this.transform.position;
-        if (pPos.x >= -293 && pPos.x <= -243 && pPos.y <= -103.0f && Input.GetKeyDown("w"))//1>2층 위로 이동
+        if (pPos.x >= -293 && pPos.x <= -243 && pPos.y <= -104.0f && Input.GetKeyDown("w"))//1>2층 위로 이동
         {
 
-            player.transform.position = new Vector3(pPos.x, 136.0f, 0f);//좌표
+            player.transform.position = new Vector3(pPos.x, 135.0f, 0f);//좌표
         }
 
-        if (pPos.x >= -293 && pPos.x <= -243 && pPos.y >= 136.0f && Input.GetKeyDown("s"))//2>1층 아래로 이동
+        if (pPos.x >= -293 && pPos.x <= -243 && pPos.y >= 135.0f && Input.GetKeyDown("s"))//2>1층 아래로 이동
         {
 
-            player.transform.position = new Vector3(pPos.x, -103.6f, 0f);//좌표
+            player.transform.position = new Vector3(pPos.x, -104.8f, 0f);//좌표
         }
-        if (pPos.x >= -293 && pPos.x <= -243 && pPos.y <= -91.0f && pPos.y >= -104.0f && Input.GetKeyDown("s"))//1>B1층 아래로 이동
+        if (pPos.x >= -293 && pPos.x <= -243 && pPos.y <= -91.0f && pPos.y >= -105.0f && Input.GetKeyDown("s"))//1>B1층 아래로 이동
         {
 
-            player.transform.position = new Vector3(pPos.x, -302.5f, 0f);//좌표
+            player.transform.position = new Vector3(pPos.x, -303.7f, 0f);//좌표
         }
         if (pPos.x >= -293 && pPos.x <= -243 && pPos.y <= -302.0f && Input.GetKeyDown("w"))//B1>1층 위로 이동
         {
@@ -45,13 +45,13 @@ public class HomePMove : MonoBehaviour
         this.player = GameObject.FindGameObjectWithTag("Player");
         Vector3 pPos = this.transform.position;
 
-        if(pPos.y >= -104.0f && pPos.y <= -102.0f) //1층 이동제한
+        if(pPos.y >= -106.0f && pPos.y <= -104.0f) //1층 이동제한
             player.transform.position = new Vector3(Mathf.Clamp(pPos.x, -432.5f, 389.04f), pPos.y, pPos.z);
 
         if(pPos.y >= 135.0f && pPos.y <= 137.0f) //2층 이동제한
             player.transform.position = new Vector3(Mathf.Clamp(pPos.x, -352.1f, 259.2f), pPos.y, pPos.z);
 
-        if(pPos.y >= -303.0f && pPos.y <= -301.0f) //B1층 이동제한
+        if(pPos.y >= -305.0f && pPos.y <= -303.0f) //B1층 이동제한
             player.transform.position = new Vector3(Mathf.Clamp(pPos.x, -321.0f, 379.2f), pPos.y, pPos.z);
     }
 }
